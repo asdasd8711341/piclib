60a46a60c3297f5175178de72d0334393aa1cd22
asdasd8711341 <995736488@qq.com>
2020-11-21 15:14:36 +0800

    优化了一下架构，

--- base-microservice-hystrix-dashboard/src/main/java/com/gsn/piclib/dashboard/HystrixDashboardApp.java
+++ base-microservice-hystrix-dashboard/src/main/java/com/gsn/piclib/dashboard/HystrixDashboardApp.java
@@ -3,12 +3,20 @@ package com.gsn.piclib.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@Controller
 public class HystrixDashboardApp {
     public static void main(String[] args) {
         SpringApplication.run(  HystrixDashboardApp.class,args );
     }
 
+    //自动将本地址的所有请求转发到  /hystrix 上.
+    @RequestMapping("/")
+    public String home() {
+        return "forward:/hystrix";
+    }
 }
